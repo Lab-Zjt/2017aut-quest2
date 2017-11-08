a83343e11efb9f5b7dc3a1683c5fc21642269780
Lab-Zjt <a1322144594@gmail.com>
2017-11-08 21:07:08 +0800

    ????

--- lab_list.h
+++ lab_list.h
@@ -1,8 +1,19 @@
 #ifndef LAB_LIST_H
 #define LAB_LIST_H
 
-struct lab__list_st{
-
+typedef int DataType;
+struct node
+{
+    DataType* data;
+    struct node* next;
+    struct node* prev;
+};
+typedef struct node Node;
+struct lab__list_st
+{
+    Node *begin;
+    Node *end;
+    int size;
 };
 
 struct lab__list_iter_st{
@@ -35,3 +46,63 @@ typedef struct lab__list_api_st ListAPI;
 extern ListAPI list;
 
 #endif
+ListDescriptor constructor()
+{
+    ListDescriptor temp;
+    temp.begin=NULL;
+    temp.end=NULL;
+    temp.size=0;
+}
+void push_back(ListDescriptor* desc,void* elem,int size)
+{
+    if(desc->end==NULL)
+    {
+        desc->end=(Node*)malloc(sizeof(Node));
+        desc->end->data=(DataType *)elem;
+        desc->begin=desc->end;
+        desc->size=1;
+        desc->end->next=NULL;
+        desc->end->prev=NULL;
+    }
+    else
+    {
+        Node* new_node=(Node*)malloc(sizeof(Node));
+        new_node->data=(DataType *)elem;
+        new_node->prev=desc->end;
+        new_node->next=NULL;
+        desc->end=new_node;
+        desc->size++;
+    }
+}
+void push_front(ListDescriptor* desc,void* elem,int size)
+{
+    if(desc->begin==NULL)
+    {
+        desc->begin=(Node*)malloc(sizeof(Node));
+        desc->begin->data=(DataType *)elem;
+        desc->end=desc->begin;
+        desc->size=1;
+        desc->begin->next=NULL;
+        desc->begin->prev=NULL;
+    }
+    else
+    {
+        Node* new_node=(Node*)malloc(sizeof(Node));
+        new_node->data=(DataType *)elem;
+        new_node->next=desc->begin;
+        new_node->prev=NULL;
+        desc->begin=new_node;
+        desc->size++;
+    }
+}
+ListIterator begin(ListDescriptor* desc);
+ListIterator end(ListDescriptor* desc);
+void* iter_dereference(ListIterator iter);
+ListIterator iter_next(ListIterator iter);
+ListIterator iter_prev(ListIterator iter);
+int size(ListDescriptor* desc);
+void pop_front(ListDescriptor* desc);
+void pop_back(ListDescriptor* desc);
+void erase(ListDescriptor* desc,ListIterator iter);
+void clear(ListDescriptor* desc);
+void destructor(ListDescriptor* desc);
--- lab_vector.h
+++ lab_vector.h
@@ -11,7 +11,7 @@ struct lab__vector_st
 
 struct lab__vector_iter_st
 {
-    struct lab__vector_st* begin;
+    DataType* ptr;
 };
 
 // NOT MODIFY BELOW
@@ -19,20 +19,20 @@ typedef struct lab__vector_st VectorDescriptor;
 typedef struct lab__vector_iter_st VectorIterator;
 
 struct lab__vector_api_st{
-    VectorDescriptor (*constructor)();//
-    void (*push_back)(VectorDescriptor* desc,void* elem,int size);//
-    VectorIterator (*begin)(VectorDescriptor* desc);//
-    VectorIterator (*end)(VectorDescriptor* desc);//
-    void* (*iter_dereference)(VectorIterator iter);//
+    VectorDescriptor (*constructor)();
+    void (*push_back)(VectorDescriptor* desc,void* elem,int size);
+    VectorIterator (*begin)(VectorDescriptor* desc);
+    VectorIterator (*end)(VectorDescriptor* desc);
+    void* (*iter_dereference)(VectorIterator iter);
     VectorIterator (*iter_move)(VectorIterator iter,int delta); // simulates: iter+8;
-    int (*size)(VectorDescriptor* desc);//
-    void (*pop_back)(VectorDescriptor* desc);//
+    int (*size)(VectorDescriptor* desc);
+    void (*pop_back)(VectorDescriptor* desc);
     void (*erase)(VectorDescriptor* desc,VectorIterator iter);
-    void (*clear)(VectorDescriptor* desc);//
-    void (*destructor)(VectorDescriptor* desc);//
-    int (*capacity)(VectorDescriptor* desc);//
-    void (*shrink_to_fit)(VectorDescriptor* desc);//
-    void (*reserve)(VectorDescriptor* desc,int resv_size);//
+    void (*clear)(VectorDescriptor* desc);
+    void (*destructor)(VectorDescriptor* desc);
+    int (*capacity)(VectorDescriptor* desc);
+    void (*shrink_to_fit)(VectorDescriptor* desc);
+    void (*reserve)(VectorDescriptor* desc,int resv_size);
 };
 
 typedef struct lab__vector_api_st VectorAPI;
@@ -42,11 +42,20 @@ extern VectorAPI vector;
 #endif
 VectorDescriptor constructor()//???????
 {
-
+    VectorDescriptor temp;
+    temp.begin=(DataType*)malloc(sizeof(DataType));
+    temp.capacity=1;
+    temp.size=1;
+    return temp;
 }
 void push_back(VectorDescriptor* desc,void* elem,int size)
 {
-    desc[size]=*elem;
+    if(desc->size>=desc->capacity)
+    {
+        reserve(desc,desc->capacity*2);
+    }
+    desc[desc->size]=*elem;
+    desc->size++;
 }
 void* iter_dereference(VectorIterator iter)//?????
 {
@@ -67,14 +76,19 @@ void destructor(VectorDescriptor* desc)
 }
 VectorIterator begin(VectorDescriptor* desc)//??????
 {
+    VectorIterator temp;
+    temp.ptr=desc->begin;
+    return temp;
 }
 VectorIterator end(VectorDescriptor* desc)//??????
 {
-
+    VectorIterator temp;
+    temp.ptr=&desc->begin[desc->size];
+    return temp;
 }
-VectorIterator iter_move(VectorIterator iter,int delta)//????
+VectorIterator iter_move(VectorIterator iter,int delta)////????
 {
-
+    iter.ptr=&iter.ptr[delta];
 }
 void pop_back(VectorDescriptor* desc)
 {
@@ -101,7 +115,20 @@ void reserve(VectorDescriptor* desc,int resv_size)
 {
     if(resv_size>desc->capacity)
     {
-        realloc(desc->begin,resv_size);
-        desc->capacity=resv_size;
+        DataType *temp = (VectorDescriptor *) malloc(sizeof(DataType) * resv_size);
+        for (int i = 0 ; i < desc->size ; i ++)
+        {
+            temp[i] = desc->begin[i];
+        }
+        DataType *t = desc->begin;
+        desc->begin = temp;
+        free(t);
+
     }
+}
+int main()
+{
+    VectorDescriptor testvd;
+    VectorAPI testvt;
+    printf("%d",testvt.begin(testvd).ptr);
 }
